38f433189741cadca7f0bd832f6e0c39ebd00c82
Vincent Xu <vincent.hao.xu@gmail.com>
2020-07-08 14:46:10 +0800

    add object merge to parsing context

--- src/context.js
+++ src/context.js
@@ -6,11 +6,21 @@ export default function context() {
 
     let models = {};
 
+    function merge(lhs, rhs) {
+        function length(s) {
+            return s ? s.length : 0;
+        }
+        if (!lhs.attributes) lhs.attributes = [];
+        if (rhs.attributes) lhs.attributes = lhs.attributes.concat(rhs.attributes);
+        if (length(lhs.desc) < length(rhs.desc)) lhs.desc = rhs.desc;
+        return lhs;
+    }
+
     return {
         result: result,
 
         model: function (model) {
-            if(models[model.id]) return models[model.id];
+            if (models[model.id]) return merge(models[model.id], model);
             result.models.push(model);
             models[model.id] = model;
             return model;
--- test/context.test.js
+++ test/context.test.js
@@ -1,12 +1,39 @@
 import context from "../src/context";
 
 describe("Parsing Context", () => {
-   test("should not create duplicated model", () => {
-       let ctx = context();
+    test("should not create duplicated model", () => {
+        let ctx = context();
 
-       ctx.model({id: 'Order'});
-       ctx.model({id: 'Order'});
+        ctx.model({id: 'Order'});
+        ctx.model({id: 'Order'});
 
-       expect(ctx.result.models.length).toBe(1);
-   });
+        expect(ctx.result.models.length).toBe(1);
+    });
+
+    test("should merge attributes with same id", () => {
+        let ctx = context();
+
+        ctx.model({id: 'Order'});
+        ctx.model({id: 'Order', attributes: [{"name": "created_at", "type": "timestamp"}]});
+
+        expect(ctx.result.models[0].attributes).toContainEqual({"name": "created_at", "type": "timestamp"});
+    });
+
+    test("should merge desc with same id", () => {
+        let ctx = context();
+
+        ctx.model({id: 'Order'});
+        ctx.model({id: 'Order', desc: 'desc'});
+
+        expect(ctx.result.models[0].desc).toBe('desc');
+    });
+
+    test("should not merge desc ''", () => {
+        let ctx = context();
+        
+        ctx.model({id: 'Order', desc: 'desc'});
+        ctx.model({id: 'Order'});
+
+        expect(ctx.result.models[0].desc).toBe('desc');
+    });
 });
