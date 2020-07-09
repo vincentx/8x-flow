982ac59e3583fff6ebb5ef75faf2eb6e379bf37b
Vincent Xu <vincent.hao.xu@gmail.com>
2020-07-09 15:23:46 +0800

    render background color according to archetype

--- d3-chart/src/model.js
+++ d3-chart/src/model.js
@@ -1,4 +1,5 @@
 import * as d3 from "d3-selection";
+import * as d3plus from "d3plus-text";
 
 export default function (chart, models, config) {
     return chart
@@ -8,11 +9,57 @@ export default function (chart, models, config) {
         .attr("class", _ => `model ${_.archetype}`);
 }
 
+const momentInterval = "rgb(229, 59, 112)";
+const role = "rgb(254, 180, 63)";
+const participant = "rgb(103, 157, 52)";
+
+const fills = [
+    [['rfp', 'proposal', 'contract', 'agreement', 'fulfillment', 'evidence', 'variform'], momentInterval],
+    [['party', 'place', 'thing', 'participant'], participant],
+    [['domain', 'system', 'role'], role]
+];
+
 function render(model) {
-    renderBackground(this);
+    renderBackground(this, model);
+    renderTextBoxes(this, model);
 }
 
-function renderBackground(container) {
+function renderTextBoxes(container, model) {
+    new d3plus.TextBox()
+        .data(texts(model))
+        .height(100)
+        .width(400)
+        .verticalAlign('middle')
+        .textAnchor('middle')
+        .fontResize(d => d.resize)
+        .fontColor('white')
+        .fontSize(d => d.font.size)
+        .fontFamily(d => d.font.family)
+        .y((d, i) => i * 100)
+        .select(container)
+        .render();
+}
+
+function texts(model) {
+    return [
+        {
+            text: ['<', model.archetype, '>'].join(' '), resize: false,
+            font: {family: '"Helvetica", sans-serif', size: 25}
+        },
+        {
+            text: model.id, resize: true,
+            font: {family: '"Helvetica", sans-serif', weight: 'bold', size: 40}
+        },
+        {
+            text: model.attributes.map(_ => _.name).join(' '), resize: false,
+            font: {family: '"Helvetica", sans-serif', size: 25}
+        }
+    ];
+}
+
+function renderBackground(container, model) {
+    let color = fills.find((f) => f[0].includes(model.archetype))[1];
+
     d3.select(container)
         .append('rect')
         .attr('class', 'background')
@@ -21,5 +68,6 @@ function renderBackground(container) {
         .attr('x', 0)
         .attr('y', 0)
         .attr('rx', 10)
-        .attr('ry', 10);
+        .attr('ry', 10)
+        .attr('fill', color);
 }
--- d3-chart/test/model.test.js
+++ d3-chart/test/model.test.js
@@ -4,25 +4,96 @@ import model from "../src/model";
 
 describe("Model rendering", () => {
 
-    test("should render model to a group", () => {
+    describe("Render single model", () => {
+        let document = dom();
+
+        beforeAll(() =>
+            model(d3.select(document.body), [{
+                id: "Order",
+                archetype: "contract",
+                attributes: [{"name": "created_at", "type": "timestamp"}, {"name": "total_price", "type": "data"}]
+            }]));
+
+        test("should render all contents", () => {
+            let content = document.querySelectorAll(".contract > .d3plus-textBox > text");
+
+            expect(content.length).toBe(3);
+            expect(content[0].innerHTML).toBe('&lt; contract &gt;');
+            expect(content[1].innerHTML).toBe('Order');
+            expect(content[2].innerHTML).toBe('created_at total_price');
+        });
+
+        test("should render background", () => {
+            let background = document.querySelectorAll(".contract > .background");
+
+            expect(background.length).toBe(1);
+            attrs(background[0], {x: 0, y: 0, rx: 10, ry: 10, width: 400, height: 300, fill: "rgb(229, 59, 112)"});
+        });
+
+        test("should align to middle", () => {
+            let texts = document.querySelectorAll(".contract > .d3plus-textBox > text");
+
+            for (let i = 0; i < texts.length; i++)
+                attrs(texts[i], {"text-anchor": "middle"});
+        });
+
+        test("should layout vertically properly", () => {
+            let boxes = document.querySelectorAll(".contract > .d3plus-textBox");
+
+            expect(boxes.length).toBe(3);
+            positions(boxes).reduce((acc, cur) => {
+                expect(cur[0]).toBe(acc[0]);
+                expect(cur[1]).toBeGreaterThan(acc[1]);
+                return cur;
+            }, [0, 0]);
+        });
+    });
+
+    let mi = "rgb(229, 59, 112)";
+    let role = "rgb(254, 180, 63)";
+    let participant = "rgb(103, 157, 52)";
+
+    test.each([
+        ['contract', mi],
+        ['fulfillment', mi],
+        ['rfp', mi],
+        ['proposal', mi],
+        ['agreement', mi],
+        ['variform', mi],
+        ['evidence', mi],
+        ['participant', participant],
+        ['party', participant],
+        ['place', participant],
+        ['thing', participant],
+        ['domain', role],
+        ['system', role],
+        ['role', role],
+    ])('Background color for archetype %s', function (archetype, color) {
         let document = dom();
         model(d3.select(document.body), [{
             id: "Order",
-            archetype: "contract",
-            attributes: []
+            archetype: archetype,
+            attributes: [{"name": "created_at", "type": "timestamp"}]
         }]);
 
-        let contracts = document.querySelectorAll(".contract");
-        expect(contracts.length).toBe(1);
+        let background = document.querySelector(`.${archetype} > .background`);
 
-        let background = contracts[0].querySelectorAll(".background");
-        expect(background.length).toBe(1);
-        attrs(background[0], {x: 0, y: 0, rx: 10, ry: 10, width: 400, height: 300});
+        attrs(background, {fill: color});
+
     });
-})
+});
+
+function positions(elements) {
+    let result = [];
+    for (let i = 0; i < elements.length; i++) {
+        let transform = elements[i].attributes.getNamedItem("transform").value;
+        let groups = transform.match(/translate\(([0-9]+),\s+([0-9]+)\)/);
+        result.push([parseFloat(groups[1]), parseFloat(groups[2])]);
+    }
+    return result;
+}
 
 function attrs(target, expected) {
-
     for (let name of Object.keys(expected)) {
         expect(attr(target, name.toString())).toBe(expected[name].toString());
     }
